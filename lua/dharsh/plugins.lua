962200fe6fca933d382407797d432310dc8408b9
Dharsh <43640229+dharshatharan@users.noreply.github.com>
2023-07-14 20:07:23 -0400

    Update copilot config to enable inline recommendations

--- lua/dharsh/plugins.lua
+++ lua/dharsh/plugins.lua
@@ -1,13 +1,18 @@
 lvim.plugins = {
   {
-    "zbirenbaum/copilot-cmp",
-    event = "InsertEnter",
-    dependencies = { "zbirenbaum/copilot.lua" },
+    "github/copilot.vim",
+    event = "VeryLazy",
     config = function()
-      vim.defer_fn(function()
-        require("copilot").setup()     -- https://github.com/zbirenbaum/copilot.lua/blob/master/README.md#setup-and-configuration
-        require("copilot_cmp").setup() -- https://github.com/zbirenbaum/copilot-cmp/blob/master/README.md#configuration
-      end, 100)
+      -- copilot assume mapped
+      vim.g.copilot_assume_mapped = true
+      vim.g.copilot_no_tab_map = true
+    end,
+  },
+  {
+    "hrsh7th/cmp-copilot",
+    config = function()
+      lvim.builtin.cmp.formatting.source_names["copilot"] = "( )"
+      table.insert(lvim.builtin.cmp.sources, 2, { name = "copilot" })
     end,
   },
   {
