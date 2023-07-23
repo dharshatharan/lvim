488a019e623af63c3a41f9010a3d307e0e20f04d
dharshatharan <dharshatharan@gmail.com>
2023-07-23 13:44:47 -0400

    Setup abosolute path from project root in telescope
    Update telescope git files config
    
    Remove rooter vim

--- lua/dharsh/telescope.lua
+++ lua/dharsh/telescope.lua
@@ -1,1 +1,25 @@
+lvim.builtin.telescope.defaults.layout_strategy = "flex"
+lvim.builtin.telescope.defaults.layout_config = {
+  horizontal = {
+    width = 0.9,
+    height = 0.9,
+    prompt_position = "top",
+  },
+  vertical = {
+    width = 0.9,
+    height = 0.9,
+  },
+}
 
+lvim.builtin.telescope.pickers.find_files = {
+  path_display = { "absolute" },
+  wrap_results = true,
+  hidden = true,
+  file_ignore_patterns = { '.git/' }
+}
+lvim.builtin.telescope.pickers.git_files = {
+  hidden = true,
+  show_untracked = true,
+  path_display = { "absolute" },
+  wrap_results = true,
+}
