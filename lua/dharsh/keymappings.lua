32c296e3abab6594b18e1185188f832dfb2fc375
dharshatharan <dharshatharan@gmail.com>
2023-08-19 00:13:01 -0400

    Add tmux-sessionizer keymap

--- lua/dharsh/keymappings.lua
+++ lua/dharsh/keymappings.lua
@@ -9,3 +9,5 @@ lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 -- Tab Navigation
 lvim.keys.normal_mode["gt"] = ":BufferLineCycleNext<CR>"
 lvim.keys.normal_mode["gT"] = ":BufferLineCyclePrev<CR>"
+
+lvim.keys.normal_mode["<C-f>"] = "<cmd>silent !tmux neww tmux-sessionizer<cr>"
